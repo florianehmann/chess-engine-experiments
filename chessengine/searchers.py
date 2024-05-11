0e429665bde37dce07c3decbced808874d898e5d
Florian Ehmann <kontakt@florianehmann.de>
2024-05-11 14:16:17 +0200

    Implement Caching

--- chessengine/searchers.py
+++ chessengine/searchers.py
@@ -1,10 +1,69 @@
 """Move tree searching functions"""
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
+from cachetools import LRUCache
 import chess
 
 from .evaluators import Evaluator
+
+
+@dataclass
+class CacheEntry:
+    """Entries of the cess cache."""
+    depth: int
+    value: float
+
+
+class ChessCache:
+    """Cache for search-enhanced evaluations of positions."""
+
+    def __init__(self, maxsize):
+        self.cache = LRUCache(maxsize=maxsize)
+        self.hits = 0
+        self.misses = 0
+
+    def contains(self, board: chess.Board, depth: int) -> bool:
+        """Checks if board is present in cache with at least the specified depth."""
+        key = self.get_cache_key(board)
+
+        if key not in self.cache:
+            self.misses += 1
+            return False
+
+        entry = self.cache[key]
+        if entry.depth < depth:
+            self.misses += 1
+            return False
+
+        self.hits += 1
+        return True
+
+    def insert_or_update(self, board: chess.Board, depth: int, value: float) -> None:
+        """Inserts the given board and depth into the cache.
+        
+        Overwrites if there already is an entry at the key generated from the board, regardless of
+        depth."""
+
+        key = self.get_cache_key(board)
+        entry = CacheEntry(depth=depth, value=value)
+        self.cache[key] = entry
+
+    def get_value(self, board: chess.Board) -> float:
+        """Retrieves the value of the board from cache at whatever depth it is stored."""
+
+        return self.cache[self.get_cache_key(board)].value
+
+    @staticmethod
+    def get_cache_key(board: chess.Board) -> str:
+        """Construncts a cache key from a board state.
+
+        Currently that includes the piece placement, active color, castling availability, and en
+        passant square, i.e., the first four entries of the FEN. The halfmove clock is omitted,
+        because it probably doesn't become relevant enough often enough to matter in caching."""
+
+        return " ".join(board.fen().split(" ")[:4])
 
 
 class Searcher(ABC):
@@ -25,24 +84,43 @@ class MinimaxSearcher(Searcher):
         return minimax(board, self.evaluator, self.depth, board.turn == chess.WHITE)
 
 
+# pylint: disable=too-few-public-methods
 class AlphaBetaSearcher(Searcher):
+    """Searcher based on Alpha-Beta Pruning"""
 
+    # pylint: disable=too-many-arguments
     def __init__(
         self,
         evaluator: Evaluator,
         depth: int,
         alpha: float = -1_000_000,
         beta: float = +1_000_000,
+        cachesize: int = 0,
     ):
         assert depth > 0
+        assert cachesize >= 0
 
         self.evaluator = evaluator
         self.depth = depth
         self.alpha = alpha
         self.beta = beta
 
+        self.cache = None
+        if cachesize > 0:
+            self.cache = ChessCache(maxsize=cachesize)
+
     def search(self, board: chess.Board) -> float:
-        return alpha_beta_search(board, self.evaluator, self.depth, self.alpha, self.beta)
+        result = None
+        if self.cache is None:
+            result = alpha_beta_search(
+                board, self.evaluator, self.depth, self.alpha, self.beta
+            )
+        else:
+            result = cached_alpha_beta_search(
+                board, self.evaluator, self.cache, self.depth, self.alpha, self.beta
+            )
+
+        return result
 
 
 def minimax(
@@ -77,6 +155,72 @@ def minimax(
                 best_value, minimax(board, evaluator, depth - 1, not maximize)
             )
             board.pop()
+
+    return best_value
+
+
+def cached_alpha_beta_search(
+    board: chess.Board,
+    evaluator: Evaluator,
+    cache: ChessCache,
+    depth: int,
+    alpha: float,
+    beta: float,
+) -> tuple[chess.Move, float]:
+
+    if depth <= 0:
+        value = None
+        if cache.contains(board, depth):
+            value = cache.get_value(board)
+        else:
+            value = evaluator.eval(board)
+            cache.insert_or_update(board, depth, value)
+        return value
+
+    if board.is_checkmate():
+        return -10_000 if board.turn == chess.WHITE else 10_000
+
+    best_value = None
+    if board.turn == chess.WHITE:
+        best_value = -99_999
+
+        for move in board.legal_moves:
+            board.push(move)
+
+            search_value = None
+            if cache.contains(board, depth - 1):
+                search_value = cache.get_value(board)
+            else:
+                search_value = alpha_beta_search(board, evaluator, depth - 1, alpha, beta)
+                cache.insert_or_update(board, depth, search_value)
+
+            best_value = max(best_value, search_value)
+
+            board.pop()
+
+            alpha = max(alpha, best_value)
+            if alpha >= beta:
+                break
+    else:
+        best_value = 99_999
+
+        for move in board.legal_moves:
+            board.push(move)
+
+            search_value = None
+            if cache.contains(board, depth - 1):
+                search_value = cache.get_value(board)
+            else:
+                search_value = alpha_beta_search(board, evaluator, depth - 1, alpha, beta)
+                cache.insert_or_update(board, depth, search_value)
+
+            best_value = min(best_value, search_value)
+
+            board.pop()
+
+            beta = min(beta, best_value)
+            if beta <= alpha:
+                break
 
     return best_value
 
